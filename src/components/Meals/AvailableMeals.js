bdaa9ac89db52433f3326acac63b2b6eff023121
rejuancse <rejuancse@gmail.com>
2023-12-08 00:59:47 +0600

    Component updated and css issue fixed

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,53 +1,65 @@
-import Card from "../UI/Card";
-import classes from "./AvailableMeals.module.css";
-import MealItem from "./MealItem/MealItem";
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+import classes from './AvailableMeals.module.css';
 
 const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
+  {
+    id: 'm1',
+    name: 'Sushi',
+    description: 'Finest fish and veggies',
+    price: 22.99,
+    image: 'https://assets-lighthouse.s3.amazonaws.com/uploads/image/file/5635/01.jpg'
+  },
+  {
+    id: 'm2',
+    name: 'Schnitzel',
+    description: 'A german specialty!',
+    price: 16.5,
+    image: 'https://assets-lighthouse.s3.amazonaws.com/uploads/image/file/5634/08.jpg'
+  },
+  {
+    id: 'm3',
+    name: 'Barbecue Burger',
+    description: 'American, raw, meaty',
+    price: 12.99,
+    image: 'https://assets-lighthouse.s3.amazonaws.com/uploads/image/file/5630/04.jpg'
+  },
+  {
+    id: 'm4',
+    name: 'Green Bowl',
+    description: 'Healthy...and green...',
+    price: 18.99,
+    image: 'https://assets-lighthouse.s3.amazonaws.com/uploads/image/file/5633/07.jpg'
+  },
+
+  {
+    id: 'm5',
+    name: 'Pizza',
+    description: 'Healthy...and green...',
+    price: 18.99,
+    image: 'https://assets-lighthouse.s3.amazonaws.com/uploads/image/file/5631/05.jpg'
+  },
 ];
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(
-        (meal) => (
-            <MealItem 
-                key={meal.id} 
-                name={meal.name} 
-                description={meal.description} 
-                price={meal.price} 
-            />
-        )
-    );
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+      image={meal.image}
+    />
+  ));
 
-    return <section className={classes.meals}>
-            <Card>
-                <ul>
-                    {mealsList}
-                </ul>
-            </Card>
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
-}
+  );
+};
 
 export default AvailableMeals;
